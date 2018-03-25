bef95f45c2497b643f47841d0e1351e53b478b59
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2018-03-25 16:55:31 +0200

    try again

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: [
         require('postcss-easy-import')({
-            'path': ['css'],
+            'path': ['themes/mft/static/css/'],
             'prefix': '_',
             'extension': 'css'
         }),
